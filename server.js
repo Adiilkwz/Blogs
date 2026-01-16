0ecd5e8830f688918421422e33b394fddff2d813
adramazan <240692@astanait.edu.kz>
2026-01-16 18:12:26 +0500

    feat: add POST /blogs endpoint for creating new posts

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const MONGO_URI = process.env.MONGO_URI
+const Blog = require('./models/blog.js')
 
 app.use(express.json());
 
@@ -14,3 +15,20 @@ mongoose
 
         app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
     })
+
+app.post('/blogs', async(req, res) => {
+    try{
+        const {title, body, author} = req.body;
+
+        if (!title || !body){
+            res.status(400).json({error: "Title and Body are required."})
+        }
+
+        const newBlog = await Blog.create({ title, body, author }) 
+
+        res.status(201).json(newBlog);
+    } catch(err){
+        console.error(err);
+        res.status(500).json({ message: "Server error" })
+    }
+});
